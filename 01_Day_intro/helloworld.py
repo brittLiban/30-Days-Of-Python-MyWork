00b426efd05ad1d2f5d2d26de1f7781d5dba9f4c
brittLiban <britt.liban@student.greenriver.edu>
2025-02-05 22:08:32 -0800

    Day 1 of Python. Looking hopefull!

---
+++ 01_Day_intro/helloworld.py
@@ -0,0 +1,22 @@
+#NOTES 
+
+# Everytime you want to print something - do it with a print()... This will lead to printing something to the console. 
+
+print(2+3)
+
+print(2%3) #This will print the remainder... NOT THE ACTUAL DIVISION... this is called MODULUS - Useful to find even or odd nums
+
+print(3//2) #This will remove the remainder and just return everything infront of the decimal
+
+print('This is a string')
+
+print(''' Python has different Data types:
+      1 - Nums (integer, Float, Complex[1 + j])
+      2 - String
+      3 - Booleans
+      4 - List - Like an array in JavaScript [Can be both 1 data type homogenous in the list or different dataTypes in the List]
+      5 - Dictionary - An unordered collection of data in a key value pair format - Like JSON 
+      6 - Tuple - Ordered collection of different data types like list but CANNOT be motified ONCE created
+      7 - Set - Is a collection of data types similar to list and typle, BUT is not an ordered list, and can only hold UNIQUE nums. (NOTHING TWICE)(ORDER NOT IMPORTANT)
+      
+      ''')
