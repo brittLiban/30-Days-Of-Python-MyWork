76596bd1d3b4b8b268f03ad89058e44a432da526
brittLiban <britt.liban@student.greenriver.edu>
2025-08-25 20:51:46 -0700

    Day 2 done!

---
+++ 02_Day_Variables/variables.py
@@ -0,0 +1,53 @@
+#Day 2L 30DaysOfPython
+
+import math
+
+
+firstName = "Liban"
+lastName = "Britt"
+fullName = firstName + " " + lastName
+country = "United States"
+city = "Kent"
+age = 22
+year = 2025
+is_married = True
+is_true = "yes"
+is_light_on = True
+var1, var2, var3, var4, var5 = "1", 2, "3", 4, 5
+
+print(type(firstName))
+print(len(firstName))
+
+num_one = 5
+num_two = 4
+
+total = num_two + num_one
+print(total)
+
+newTotal = num_two - num_one
+print(newTotal)
+
+newerTotal = num_two % num_one
+print(newerTotal)
+
+exp = num_two ** num_one
+print(exp)
+
+floorDiv = num_two // num_one
+print(floorDiv)
+
+#area of circle
+
+r = 30
+
+areaOfCircle = math.pi * (r**2)
+print(areaOfCircle)
+
+circumfranceOfCircle = 2 * math.pi * r
+print(circumfranceOfCircle)
+
+first_name = input("What is your first name")
+last_name = input("what is your last name")
+
+age = input("What is your age? ")
+
